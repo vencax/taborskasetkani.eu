f56802e32b17f55302ee507415fdd2a96295b101
Your Name <you@example.com>
2021-05-17 19:47:01 +0200

    deletions

--- _components/triangleWrapper.js
+++
@@ -1,9 +0,0 @@
-export default {
-  props: ['data'],
-  template: `
-  <div class="triangle-wrapper">    
-    <div class="left" :class="(data ? data.left : null) || 'red'"></div>
-    <div class="right" :class="(data ? data.right : null) || 'yellow'"></div>
-  </div>
-  `
-}
